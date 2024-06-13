96b860fa96019213fcb6f299464fb71b987c646f
Chandan <cchuphal4@gmail.com>
2024-06-13 15:08:26 +0530

    node-cached feature is added

--- backend/controllers/recipe.js
+++ backend/controllers/recipe.js
@@ -1,4 +1,6 @@
 import { logger } from "../utils/logger.js";
+import NodeCache from "node-cache";
+
 import {
   BadRequestError,
   CustomAPIError,
@@ -7,13 +9,23 @@ import {
 import Recipe from "../models/Recipe.js";
 import { StatusCodes } from "http-status-codes";
 
+const myCache = new NodeCache();
+
 export const getAllRecipes = async (req, res) => {
   const { page = 1 } = req.query;
   const offSet = (page - 1) * 10;
-  const recipes = await Recipe.find()
-    .sort({ createdAt: -1 })
-    .skip(offSet)
-    .limit(10);
+
+  let recipes = [];
+  if (myCache.has("recipes" + "_" + String(page))) {
+    recipes = JSON.parse(myCache.get("recipes" + "_" + String(page)));
+  } else {
+    recipes = await Recipe.find()
+      .sort({ createdAt: -1 })
+      .skip(offSet)
+      .limit(10);
+
+    myCache.set("recipes" + "_" + String(page), JSON.stringify(recipes), 600);
+  }
 
   if (recipes.length === 0) {
     logger.error("Currently, No recipe is available");
@@ -59,6 +71,11 @@ export const createRecipe = async (req, res) => {
 
   req.body.createdBy = req.user.userId;
   const recipe = await Recipe.create(req.body);
+
+  const keys = myCache.keys();
+  const filtered_keys = keys.filter((key) => key.startsWith("recipes" + "_"));
+  filtered_keys.map((key) => myCache.del(key));
+
   logger.info("Recipe created Successfully");
   res
     .status(StatusCodes.CREATED)
@@ -67,15 +84,21 @@ export const createRecipe = async (req, res) => {
 
 export const getSingleRecipe = async (req, res) => {
   const { id } = req.params;
+  let recipe = [];
+  if (myCache.has("recipe" + "#" + id)) {
+    recipe = myCache.get("recipe" + "#" + id);
+  } else {
+    recipe = await Recipe.findOne({
+      _id: id,
+    });
 
-  const recipe = await Recipe.findOne({
-    _id: id,
-  });
-
-  if (!recipe) {
-    logger.error(`No recipe with id ${id}`);
-    throw new NotFoundError(`No recipe with id ${id}`);
+    if (!recipe) {
+      logger.error(`No recipe with id ${id}`);
+      throw new NotFoundError(`No recipe with id ${id}`);
+    }
+    myCache.set("recipe" + "#" + id, recipe, 600);
   }
+
   logger.info("Successfully get the recipe");
   res.status(StatusCodes.OK).json({
     msg: "Successfully get the recipe",
@@ -126,11 +149,17 @@ export const updateRecipe = async (req, res) => {
     logger.error("Please provide image regarding the recipe");
     throw new BadRequestError("Please provide image regarding the recipe");
   }
+  myCache.del("recipe" + "#" + id);
+
+  const keys = myCache.keys();
+  const filtered_keys = keys.filter((key) => key.startsWith("recipes" + "_"));
+  filtered_keys.map((key) => myCache.del(key));
 
   const newRecipe = await Recipe.findByIdAndUpdate({ _id: id }, req.body, {
     new: true,
     runValidators: true,
   });
+
   logger.info("Successfully updated the Recipe");
   res
     .status(StatusCodes.OK)
@@ -139,8 +168,9 @@ export const updateRecipe = async (req, res) => {
 
 export const deleteRecipe = async (req, res) => {
   const { id } = req.params;
+  const user_Id = req.user.userId;
 
-  const recipe = await Recipe.findByIdAndDelete({
+  const recipe = await Recipe.findOne({
     _id: id,
   });
 
@@ -149,7 +179,23 @@ export const deleteRecipe = async (req, res) => {
     throw new NotFoundError(`No recipe with id ${id}`);
   }
 
+  myCache.del("recipe" + "#" + id);
+
+  const keys = myCache.keys();
+  const filtered_keys = keys.filter((key) => key.startsWith("recipes" + "_"));
+  filtered_keys.map((key) => myCache.del(key));
+
+  if (user_Id != recipe.createdBy) {
+    logger.error("Authorization-Invalid.");
+    throw new CustomAPIError("Authorization-Invalid.", StatusCodes.FORBIDDEN);
+  }
+
+  await Recipe.findByIdAndDelete({
+    _id: id,
+  });
+
   logger.info("Successfully deleted recipe");
+
   res
     .status(StatusCodes.OK)
     .json({ msg: "Successfully deleted recipe", recipe });
